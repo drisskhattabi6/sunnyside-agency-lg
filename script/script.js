184b11d0c18937c60d44f6b63cf533e8d66ea429
idriss khattabi <49597702+drisskhattabi6@users.noreply.github.com>
2023-02-01 20:46:47 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const barBtn = document.getElementById('bar-btn')
+const nav2 = document.querySelector('.nav2')
+
+barBtn.addEventListener('click', function () {
+    nav2.classList.toggle('active')
+})
